cb9ef18d98b10c7f15aaf7072b1a09826d162f85
LO48 <lo48@me.com>
2021-08-30 18:55:45 -0700

    example 1

--- src/main/java/com/csc205/Hello.java
+++ src/main/java/com/csc205/Hello.java
@@ -3,6 +3,8 @@ package com.csc205;
 public class Hello {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        System.out.println("example 1: ");
     }
 }
